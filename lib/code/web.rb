5618f5c8e7aeb7abfc432bdd8d0411c195226dd4
Noah Zoschke <noah@heroku.com>
2011-11-29 18:05:27 -0800

    refactor redirect code

--- lib/code/web.rb
+++ lib/code/web.rb
@@ -6,19 +6,26 @@ module Code
     def exchange
       @exchange ||= Code::Exchange.new
     end
+
+    def forward!(hostname)
+      url  = "http://#{hostname}"
+      url += env["PATH_INFO"]
+      url += env["QUERY_STRING"] unless env["QUERY_STRING"].empty?
+      redirect url, 302
+    end
   end
 
   class Web < Sinatra::Application
     helpers Helpers
 
     get "/:app_name.git/info/refs" do
-      d = exchange.exchange("backend.cedar", {app_name: params[:app_name]})
-      redirect "https://#{d[:hostname]}/code.git/info/refs", 302
+      d = exchange.exchange("backend.cedar", params[:app_name], {app_name: params[:app_name]})
+      forward! d[:hostname]
     end
 
     post "/:app_name.git/git-receive-pack" do
       d = exchange.get(params[:app_name])
-      redirect "https://#{d[:hostname]}/code.git/git-receive-pack", 302
+      forward! d[:hostname]
     end
   end
 end
--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -19,18 +19,18 @@ describe Code::Web do
   end
 
   it "requests info for a repo and redirects to a backend" do
-    @ex.should_receive(:exchange).with("backend.cedar", {:app_name => "code"}).and_return(hostname: "route.heroku.com:3333")
+    @ex.should_receive(:exchange).with("backend.cedar", "code", {:app_name => "code"}).and_return(hostname: "route.heroku.com:3333")
 
     get "/code.git/info/refs"
     last_response.status.should == 302
-    last_response.location.should == "https://route.heroku.com:3333/code.git/info/refs"
+    last_response.location.should == "http://route.heroku.com:3333/code.git/info/refs"
   end
 
   it "posts a pack to a repo and redirects to a backend" do
     @ex.should_receive(:get).with("code").and_return(hostname: "route.heroku.com:3333")
 
-    post "/code.git/git-receive-pack?foo=bar"
+    post "/code.git/git-receive-pack"
     last_response.status.should == 302
-    last_response.location.should == "https://route.heroku.com:3333/code.git/git-receive-pack"
+    last_response.location.should == "http://route.heroku.com:3333/code.git/git-receive-pack"
   end
 end
