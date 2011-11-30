4d1d45860290263ac9a920d2a781e834a53a276a
Noah Zoschke <noah@heroku.com>
2011-11-30 14:46:28 -0800

    simulate starting a process with a new pid

--- lib/code/monitor.rb
+++ lib/code/monitor.rb
@@ -6,5 +6,11 @@ module Code
       @num_processes = ENV["NUM_PROCESSES"] || 5
       @processes = []
     end
+
+    def start(cmd, env={})
+      pid = (5000..6000).to_a.sample
+      @processes << pid
+      pid
+    end
   end
 end
