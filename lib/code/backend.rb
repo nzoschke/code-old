91e151fbb859e9047ba688b6024d047e724f3917
Noah Zoschke <noah@heroku.com>
2011-12-01 01:11:31 -0800

    refactor web apps into Web module

--- lib/code/backend.rb
+++ lib/code/backend.rb
@@ -6,35 +6,37 @@ $root_dir = File.expand_path(File.join(__FILE__, "..", "..", ".."))
 $work_dir = "/app"
 
 module Code
-  class Backend < Sinatra::Application
+  module Web
+    class Backend < Sinatra::Application
 
-    GIT = GitHttp::App.new({
-      :project_root => "#{$work_dir}",
-      :upload_pack  => true,
-      :receive_pack => true,
-    })
+      GIT = GitHttp::App.new({
+        :project_root => "#{$work_dir}",
+        :upload_pack  => true,
+        :receive_pack => true,
+      })
 
-    def call(env)
-      GIT.call(env)
-    end
+      def call(env)
+        GIT.call(env)
+      end
 
-    def self.monitor_exchange
-      ex = Exchange.new
-      begin
-        puts "DEQUEING..."
-        data = ex.dequeue("backend.cedar", timeout: 10)
-      end while !data
+      def self.monitor_exchange
+        ex = Exchange.new
+        begin
+          puts "DEQUEING..."
+          data = ex.dequeue("backend.cedar", timeout: 10)
+        end while !data
 
-      puts `bin/unstow-repo "#{data[:repo_get_url]}" #{$work_dir}`
+        puts `bin/unstow-repo "#{data[:repo_get_url]}" #{$work_dir}`
 
-      ex.reply(data)
+        ex.reply(data)
 
-      begin
-        puts "MONITORING..."
-        flag = File.exists? "#{$work_dir}/.log/finished"
-        sleep 5
-      end while !flag
-      Process.kill("TERM", $$)
+        begin
+          puts "MONITORING..."
+          flag = File.exists? "#{$work_dir}/.log/finished"
+          sleep 5
+        end while !flag
+        Process.kill("TERM", $$)
+      end
     end
   end
 end
