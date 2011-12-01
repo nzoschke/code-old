899c47ade110eb6f905a4860519467063c0238a5
Noah Zoschke <noah@heroku.com>
2011-11-30 21:07:22 -0800

    get a push working! hook up unstow script, init a bare repo, git_http ignores path for convention

--- lib/code/backend.rb
+++ lib/code/backend.rb
@@ -2,13 +2,14 @@ require "sinatra"
 require "./lib/code"
 require "./lib/git_http"
 
-$root = File.expand_path(File.join(__FILE__, "..", "..", ".."))
+$root_dir = File.expand_path(File.join(__FILE__, "..", "..", ".."))
+$work_dir = "/app"
 
 module Code
   class Backend < Sinatra::Application
 
     GIT = GitHttp::App.new({
-      :project_root => "#{$root}/tmp",
+      :project_root => "#{$work_dir}",
       :upload_pack  => true,
       :receive_pack => true,
     })
@@ -24,14 +25,7 @@ module Code
         data = ex.dequeue("backend.cedar", timeout: 10)
       end while !data
 
-      puts data.inspect
-      puts <<`EOF`
-        set -x
-        TMP_DIR=#{$root}/tmp
-        rm -rf $TMP_DIR
-        mkdir -p $TMP_DIR
-        git init --bare $TMP_DIR/code.git
-EOF
+      puts `bin/unstow-repo "#{data[:repo_get_url]}" #{$work_dir}`
 
       ex.reply(data)
     end
