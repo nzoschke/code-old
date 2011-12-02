551dd43954781e5f4b329e82166271320182e1e0
Noah Zoschke <noah@heroku.com>
2011-12-02 13:54:54 -0800

    code -> code-staging in spec

--- spec/director_spec.rb
+++ spec/director_spec.rb
@@ -17,18 +17,18 @@ describe "Code::Web::Director" do
 
   it "requests info for a repo and redirects to a backend" do
     @ex.should_receive(:hostname).and_return("code.heroku.com")
-    @ex.should_receive(:exchange).with("backend.cedar", hash_including(app_name: "code"), {:name => "code"}).and_return(hostname: "route.heroku.com:3333")
+    @ex.should_receive(:exchange).with("backend.cedar", hash_including(app_name: "code-staging"), {:name => "code-staging"}).and_return(hostname: "route.heroku.com:3333")
 
-    get "/code.git/info/refs"
+    get "/code-staging.git/info/refs"
     last_response.status.should == 302
-    last_response.location.should == "http://route.heroku.com:3333/code.git/info/refs"
+    last_response.location.should == "http://route.heroku.com:3333/code-staging.git/info/refs"
   end
 
   it "posts a pack to a repo and redirects to a backend" do
-    @ex.should_receive(:get).with("code").and_return(hostname: "route.heroku.com:3333")
+    @ex.should_receive(:get).with("code-staging").and_return(hostname: "route.heroku.com:3333")
 
-    post "/code.git/git-receive-pack"
+    post "/code-staging.git/git-receive-pack"
     last_response.status.should == 302
-    last_response.location.should == "http://route.heroku.com:3333/code.git/git-receive-pack"
+    last_response.location.should == "http://route.heroku.com:3333/code-staging.git/git-receive-pack"
   end
 end
