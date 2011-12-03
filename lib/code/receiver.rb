41bcb6f0a18b740293e07eca6f5f85bad381dfa6
Noah Zoschke <noah@heroku.com>
2011-12-03 10:01:17 -0800

    refactor receiver, data reader; instrument all methods

--- lib/code/receiver.rb
+++ lib/code/receiver.rb
@@ -4,25 +4,26 @@ require "./lib/code"
 module Code
   class Receiver
     instrumentable do
+      attr_reader :data, :exchange
+    
       def initialize
         @exchange = Exchange.new
 
-        data = monitor_exchange
-        unstow_repo(data)
-        @exchange.reply(data)
+        monitor_exchange
+        unstow_repo
+        reply_exchange
         monitor_git
-        stow_repo(data)
+        stow_repo
         self_destruct
       end
 
       def monitor_exchange
         begin
-          data = @exchange.dequeue("backend.cedar", timeout: 10)
-        end while !data
-        data
+          @data = @exchange.dequeue("backend.cedar", timeout: 10)
+        end while !@data
       end
-    
-      def unstow_repo(data)
+
+      def unstow_repo
         `bin/unstow-repo #{WORK_DIR} "#{data[:metadata]["repo_get_url"]}"`
 
         # persist metadata and env to the disk
@@ -35,6 +36,10 @@ module Code
         end
       end
 
+      def reply_exchange
+        exchange.reply(data)
+      end
+
       def monitor_git
         begin
           puts "MONITORING..."
@@ -43,7 +48,7 @@ module Code
         end while !flag
       end
 
-      def stow_repo(data)
+      def stow_repo
         `bin/stow-repo #{WORK_DIR} "#{data[:metadata]["repo_put_url"]}"`
         `bin/post-logs #{WORK_DIR} "#{data[:push_api_url]}"`
       end
@@ -55,7 +60,9 @@ module Code
 
     Log.instrument(self, :monitor_exchange)
     Log.instrument(self, :unstow_repo)
+    Log.instrument(self, :reply_exchange)
     Log.instrument(self, :monitor_git)
     Log.instrument(self, :stow_repo)
+    Log.instrument(self, :self_destruct)
   end
 end
