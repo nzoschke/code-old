32d83b92bbb547a6fde9f6e3de009fdde3bccbbf
Noah Zoschke <noah@heroku.com>
2011-12-04 22:00:45 -0800

    debug logs on self-destruct

--- lib/code/receiver.rb
+++ lib/code/receiver.rb
@@ -72,6 +72,7 @@ module Code
       end
 
       def self_destruct
+        puts `cat #{WORK_DIR}/.tmp/*`
         `bin/post-logs #{WORK_DIR} "#{data[:push_api_url]}"`
         Process.kill("TERM", $$)
       end
