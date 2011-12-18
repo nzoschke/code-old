951437db7ad3467a937558d50ae8fc6697a40c72
Noah Zoschke <noah@heroku.com>
2011-12-17 19:37:59 -0800

    send LOG_TOKEN to build env

--- lib/code/receiver.rb
+++ lib/code/receiver.rb
@@ -57,7 +57,11 @@ module Code
         # persist metadata and env to the disk
         File.open("#{WORK_DIR}/.tmp/metadata.yml", "w") { |f| f.write YAML.dump data[:metadata] }
         File.open("#{WORK_DIR}/.tmp/build_env", "w") do |f|
-          data[:metadata]["env"].merge("PATH" => ENV["PATH"], "APP_DIR" => APP_DIR).each do |k,v|
+          data[:metadata]["env"].merge(
+            "APP_DIR"   => APP_DIR,
+            "LOG_TOKEN" => ENV["LOG_TOKEN"], 
+            "PATH"      => ENV["PATH"]
+          ).each do |k,v|
             v = v.gsub(/'/, "\\\\'")  # escape any single quotes with backslash
             f.write("#{k}=$'#{v}'\n") # use bash $'...' ANSI-C quoting
           end
